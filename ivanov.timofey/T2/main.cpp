715b49d84d5a8b7d383e1cf525be6018b396399f
Kabisdox <timoveyivanov@yandex.ru>
2026-06-07 20:41:00 +0300

    -

--- ivanov.timofey/T2/main.cpp
+++ ivanov.timofey/T2/main.cpp
@@ -3,6 +3,7 @@
 #include <iterator>
 #include <vector>
 #include <algorithm>
+#include <limits>
 
 int main()
 {
@@ -10,11 +11,20 @@ int main()
 
     std::vector<DataStruct> data;
 
-    std::copy(
-        std::istream_iterator<DataStruct>(std::cin),
-        std::istream_iterator<DataStruct>(),
-        std::back_inserter(data)
-    );
+    while (!std::cin.eof())
+    {
+        std::copy(
+            std::istream_iterator<DataStruct>(std::cin),
+            std::istream_iterator<DataStruct>(),
+            std::back_inserter(data)
+        );
+
+        if (std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+    }
 
     std::sort(data.begin(), data.end(), comparator);
 
