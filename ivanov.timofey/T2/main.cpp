105263cb463de1779d7966839d44265ef5e3458f
Kabisdox <timoveyivanov@yandex.ru>
2026-06-07 20:32:47 +0300

    .

--- ivanov.timofey/T2/main.cpp
+++ ivanov.timofey/T2/main.cpp
@@ -10,17 +10,11 @@ int main()
 
     std::vector<DataStruct> data;
 
-    while (!std::cin.eof()) {
-        std::copy(
-            std::istream_iterator<DataStruct>(std::cin),
-            std::istream_iterator<DataStruct>(),
-            std::back_inserter(data)
-        );
-        if (std::cin.fail()) {
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        }
-    }
+    std::copy(
+        std::istream_iterator<DataStruct>(std::cin),
+        std::istream_iterator<DataStruct>(),
+        std::back_inserter(data)
+    );
 
     std::sort(data.begin(), data.end(), comparator);
 
